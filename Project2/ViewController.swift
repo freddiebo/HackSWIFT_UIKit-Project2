f23a40e15f4ead8f1a3de51d021a58596d5ba377
VBondarev <freddiebo@yandex.ru>
2019-12-06 23:51:56 +0300

    Project2

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -10,11 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var scoreLable: UILabel!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    var numQuest = 1
+    var countQuest = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scoreLable.text = "Your score: \(score)"
+        Button1.layer.borderWidth = 1
+        Button1.layer.borderColor = UIColor.lightGray.cgColor
+        
+        Button2.layer.borderWidth = 1
+        Button2.layer.borderColor = UIColor.lightGray.cgColor
+        
+        Button3.layer.borderWidth = 1
+        Button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
+        
+        title = "\(numQuest) of \(countQuest): \(countries[correctAnswer].uppercased())"
+        
+    }
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        
+        Button1.setImage(UIImage(named: countries[0]), for: .normal)
+        Button2.setImage(UIImage(named: countries[1]), for: .normal)
+        Button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        title = "\(numQuest) of \(countQuest): \(countries[correctAnswer].uppercased())"
+        scoreLable.text = "Your score: \(score)"
+        
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if (sender.tag == correctAnswer) {
+            title = "Correct"
+            score += 1
+        }
+        else {
+            title = "Wrong"
+            score -= 1
+            let ac = UIAlertController(title: title, message: "This is \(countries[sender.tag].uppercased()).", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default))
+            present(ac, animated: true)
+        }
+        if numQuest == countQuest {
+            numQuest = 1
+            let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Restart", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+            score = 0
+        }
+        else {
+            numQuest += 1
+            askQuestion()
+        }
+    }
 }
 
