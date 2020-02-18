df60e6abfd79b2a652eaf4bc938f325bd7f2ade9
VBondarev <freddiebo@yandex.ru>
2020-02-18 07:52:47 +0300

    Add save best score with NSCodable

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var correctAnswer = 0
     var score = 0
+    var bestScore: Int = 0
     var numQuest = 1
     var countQuest = 10
     
@@ -25,6 +26,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         scoreLable.text = "Your score: \(score)"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(showScore))
         Button1.layer.borderWidth = 1
         Button1.layer.borderColor = UIColor.lightGray.cgColor
         
@@ -67,14 +70,50 @@ class ViewController: UIViewController {
         }
         if numQuest == countQuest {
             numQuest = 1
-            let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+            load()
+            print("Your score is \(score). The best: \(bestScore)")
+            let ac = UIAlertController(title: title, message: "Your score is \(score). The best: \(bestScore)", preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "Restart", style: .default, handler: askQuestion))
             present(ac, animated: true)
+            if score > bestScore {
+                save()
+                print("Your score is \(score). The best: \(bestScore)")
+            }
             score = 0
         }
         else {
             numQuest += 1
             askQuestion()
+        }
+    }
+    
+    @objc func showScore() {
+        let vc = UIAlertController(title: "Info", message: "Your score is \(score).", preferredStyle: .alert)
+        vc.addAction(UIAlertAction(title: "Close", style: .cancel))
+        present(vc, animated: true)
+    }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(score) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "BestScore")
+        } else {
+            print("Failed to save people.")
+        }
+    }
+    
+    func load() {
+        let defaults = UserDefaults.standard
+
+        if let savedScore = defaults.object(forKey: "BestScore") as? Data {
+            let jsonDecoder = JSONDecoder()
+
+            do {
+                bestScore = try jsonDecoder.decode(Int.self, from: savedScore)
+            } catch {
+                print("Failed to load people")
+            }
         }
     }
 }
